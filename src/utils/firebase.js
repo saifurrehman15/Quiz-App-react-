f2aee5689dccb92e87b0efd308785e388c21adef
saifurrehman15 <saifkhandada72@gmail.com>
2026-04-03 16:58:47 +0500

    Update firebase.js

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDh3AkTF-zbHFeuWEtvYcp1Mzz8igva-S0",
   authDomain: "ffaa-1a5f9.firebaseapp.com",
   projectId: "ffaa-1a5f9",
-  storageBucket: "ffaa-1a5f9.firebasestorage.app",
+  storageBucket: "ffaa-1a5f9.appspot.com" ,  
   messagingSenderId: "813877709420",
   appId: "1:813877709420:web:94a2abe8058b865b93a649",
   measurementId: "G-TE2XV3LQHP"
